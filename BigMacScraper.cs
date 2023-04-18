a66325862d910d630c34cc56c25bb4f8783a3fe2
Emilia Hansson <eh222yn@student.lnu.se>
2023-04-18 16:33:44 +0200

    Create a BigMacScraper class

---
+++ BigMacScraper.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace assignment_wt2_oauth
+{
+    public class BigMacScraper : IBigMacScraper
+    {
+        public async Task<IEnumerable<object>> GetData()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
