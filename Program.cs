9cb37fed948616283a8187b3a45c7132245455a3
Emilia Hansson <eh222yn@student.lnu.se>
2023-04-19 10:23:00 +0200

    Add code to handle environment variables

--- Program.cs
+++ Program.cs
@@ -1,5 +1,13 @@
 ﻿using assignment_wt2_oauth;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+
+// https://stackoverflow.com/questions/39573571/net-core-console-application-how-to-configure-appsettings-per-environment
+var builder = new ConfigurationBuilder()
+  .SetBasePath(Directory.GetCurrentDirectory())
+  .AddJsonFile($"appsettings.json", true, true);
+
+var configuration = builder.Build();
 
 var serviceProvider = new ServiceCollection()
   .AddSingleton<IBigMacScraper, BigMacScraper>()
