a761cf1511e14e6854d9604e459e6b096236b622
Emilia Hansson <eh222yn@student.lnu.se>
2023-04-20 17:44:08 +0200

    Add basic model for price

---
+++ big-mac-api/Models/PriceModel.cs
@@ -0,0 +1,9 @@
+public class PriceModel
+{
+    public string Date { get; set; }
+    public string CurrencyCode { get; set; }
+    public string Name { get; set; }
+    public double LocalPrice { get; set; }
+    public int DollarEx { get; set; }
+    public double DollarPrice { get; set; }
+}
