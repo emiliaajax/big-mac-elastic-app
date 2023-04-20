af7e5f681402bc1dc25619459f8a0bcd4abceced
Emilia Hansson <eh222yn@student.lnu.se>
2023-04-20 17:45:34 +0200

    Create elastic client

--- big-mac-api/Program.cs
+++ big-mac-api/Program.cs
@@ -1,3 +1,6 @@
+using Elasticsearch.Net;
+using Nest;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,6 +11,28 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// https://stackoverflow.com/questions/39573571/net-core-console-application-how-to-configure-appsettings-per-environment
+var configBuilder = new ConfigurationBuilder()
+  .SetBasePath(Directory.GetCurrentDirectory())
+  .AddJsonFile($"appsettings.json", true, true);
+
+var configuration = configBuilder.Build();
+
+var url = configuration["ElasticSearch:URL"];
+var username = configuration["ElasticSearch:Username"];
+var password = configuration["ElasticSearch:Password"];
+
+var connectionSettings = new ConnectionSettings(new Uri(url));
+connectionSettings.DisableDirectStreaming();
+connectionSettings.BasicAuthentication(username, password);
+connectionSettings.ServerCertificateValidationCallback(CertificateValidations.AllowAll);
+
+var elasticClient = new ElasticClient(connectionSettings);
+
+var serviceProvider = new ServiceCollection()
+  .AddSingleton<IElasticClient>(elasticClient)
+  .BuildServiceProvider();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
