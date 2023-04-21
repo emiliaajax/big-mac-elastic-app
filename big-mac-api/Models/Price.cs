c838b18ff93160ef20f949a1b66a2552b57ca5c2
Emilia Hansson <eh222yn@student.lnu.se>
2023-04-21 14:11:30 +0200

    Change name of class and change name of props

---
+++ big-mac-api/Models/Price.cs
@@ -0,0 +1,10 @@
+public class Price
+{
+    public string date { get; set; }
+    public string currency_code { get; set; }
+    public string name { get; set; }
+    public double local_price { get; set; }
+    public int dollar_ex { get; set; }
+    public double dollar_price { get; set; }
+    public DateTime TimeStamp { get; set; }
+}
--- big-mac-api/Models/PriceModel.cs
+++
@@ -1,9 +0,0 @@
-public class PriceModel
-{
-    public string Date { get; set; }
-    public string CurrencyCode { get; set; }
-    public string Name { get; set; }
-    public double LocalPrice { get; set; }
-    public int DollarEx { get; set; }
-    public double DollarPrice { get; set; }
-}
