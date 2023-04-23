d8ebfc2e18cca6cd7fd6a86c9115d9267d2add85
Emilia Hansson <eh222yn@student.lnu.se>
2023-04-23 17:26:37 +0200

    Add methods for getting data from API

---
+++ client/src/features/bigMacPrices/pricesService.js
@@ -0,0 +1,21 @@
+import axios from 'axios'
+
+const getPrices = async () => {}
+
+const getPricesForCountry = async () => {}
+
+const getCountries = async () => {}
+
+const getMostExpensiveCountries = async () => {}
+
+const getCheapestCountries = async () => {}
+
+const pricesService = {
+  getPrices,
+  getPricesForCountry,
+  getCountries,
+  getMostExpensiveCountries,
+  getCheapestCountries
+}
+
+export default pricesService
